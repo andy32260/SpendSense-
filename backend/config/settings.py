dc6c7712ad7dffca27d2d9a3474ac92d2121a7c4
andy32260 <andyhoang991@gmail.com>
2026-09-10 10:24:24 +0100

    Fix CORS configuration to allow frontend requests to backend

--- backend/config/settings.py
+++ backend/config/settings.py
@@ -41,9 +41,16 @@ INSTALLED_APPS = [
     'transactions',
     'budgets',
     'savings',
+    'corsheaders',
+]
+
+CORS_ALLOWED_ORIGINS = [
+    "http://localhost:5173",
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
