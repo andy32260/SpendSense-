9f4ad7d038133f745671ffa2f9317c622a983de8
andy32260 <andyhoang991@gmail.com>
2026-09-14 18:32:23 +0100

    Build Dashboard displaying monthly summary and recurring transaction detection

--- frontend/src/api/transactions.ts
+++ frontend/src/api/transactions.ts
@@ -12,3 +12,28 @@ export async function getTransactions(): Promise<Transaction[]> {
   const response = await apiClient.get('transactions/');
   return response.data;
 }
+
+export interface MonthlySummaryItem {
+  category__name: string;
+  total: string;
+}
+
+export async function getMonthlySummary(year: number, month: number): Promise<MonthlySummaryItem[]> {
+  const response = await apiClient.get('transactions/summary/', {
+  params: { year, month }});
+  return response.data;
+}
+
+export interface RecurringTransactionItem {
+  key: string;
+  confidence_score: number;
+  avg_amount: number;
+  interval_score: number;
+  avg_interval_days: number;
+  occurrences: number;
+}
+
+export async function getRecurringTransactions(): Promise<RecurringTransactionItem[]> {
+  const response = await apiClient.get('transactions/recurring/');
+  return response.data;
+}
--- frontend/src/pages/Dashboard.tsx
+++ frontend/src/pages/Dashboard.tsx
@@ -1,3 +1,33 @@
+import {useState, useEffect} from 'react'
+import { getMonthlySummary, type MonthlySummaryItem, getRecurringTransactions, type RecurringTransactionItem } from '../api/transactions';
+
 export default function Dashboard() {
-  return <h1>Dashboard</h1>;
+    const [monthlysummaryitem, setMonthlySummaryItem] = useState<MonthlySummaryItem[]>([]);
+    const [recurringtransactionitem, setRecurringTransaction] = useState<RecurringTransactionItem[]>([]);
+    
+
+    useEffect(() => {
+    async function fetchData() {
+        const monthlySummary = await getMonthlySummary(2026, 8);
+        setMonthlySummaryItem(monthlySummary);
+        const recurringTransaction = await getRecurringTransactions();
+        setRecurringTransaction(recurringTransaction);
+    }
+    fetchData();
+    }, []);
+    
+    return (
+    <div>
+      {
+        <ul>
+            {monthlysummaryitem.map((mnnth_sum) => (
+                <li key={mnnth_sum.category__name}>{mnnth_sum.category__name} - £{mnnth_sum.total}</li>
+            ))}
+            {recurringtransactionitem.map((recurr_tran) => (
+              <li key={recurr_tran.key}>{recurr_tran.occurrences} - £{recurr_tran.avg_amount}</li>
+            ))}
+        </ul>
+      }
+    </div>
+  );
 }
