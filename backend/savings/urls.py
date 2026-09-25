6dfb110235376af0fa84a6be6c2ad946ca03ef54
andy32260 <andyhoang991@gmail.com>
2026-09-25 21:44:30 +0100

    Add savings projection API endpoint

--- backend/savings/urls.py
+++ backend/savings/urls.py
@@ -1,7 +1,10 @@
 from rest_framework.routers import DefaultRouter
-from .views import SavingsGoalViewSet
+from .views import SavingsGoalViewSet, SavingsProjectionView
+from django.urls import path
 
 router = DefaultRouter()
 router.register(r'savings-goals', SavingsGoalViewSet, basename='savings-goals')
 
-urlpatterns = router.urls
+urlpatterns = [
+    path('savings-goals/projection/', SavingsProjectionView.as_view(), name='savings-projection'),
+] + router.urls
--- backend/savings/views.py
+++ backend/savings/views.py
@@ -1,6 +1,10 @@
 from rest_framework import viewsets
 from .models import SavingsGoal
+from transactions.models import Category
 from .serializers import SavingsGoalSerializer
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .services import calculate_projection
 
 class SavingsGoalViewSet(viewsets.ModelViewSet):
     serializer_class = SavingsGoalSerializer
@@ -10,3 +14,19 @@ class SavingsGoalViewSet(viewsets.ModelViewSet):
 
     def perform_create(self, serializer):
         serializer.save(user=self.request.user)
+
+class SavingsProjectionView(APIView):
+    def post(self, request):
+        savings_goal_id = request.data.get('savings_goal_id')
+        savings_goal = SavingsGoal.objects.get(id=savings_goal_id, user=request.user)
+        data = []
+        for i in request.data.get('reductions'):
+            category_id = i.get('category_id')
+            category = Category.objects.get(id=category_id, user=request.user)
+            percentage = i.get('percentage')
+            data.append((category, percentage))   
+
+        result = calculate_projection(savings_goal, data)
+        return Response(result)
+
+
